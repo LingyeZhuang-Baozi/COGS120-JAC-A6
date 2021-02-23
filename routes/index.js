57fd5a36a51894a4adebac2e213fbb5dae808c6c
Lingye Zhuang <l1zhuang@ucsd.edu>
2021-02-22 19:29:30 -0800

    data → JSON

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,8 @@
 /*
  * GET home page.
  */
+var class_data = require('../JSON/class_data.json');
 exports.view = function(req, res){
-  res.render("index",{
-  	"classname": [
-  	{
-  		"class": "COGS 120",
-  		"id": "class1"
-  	},
-  	{
-  		"class": "COGS 100",
-  		"id" : "class2"
-  	},
-  	{
-  		"class": "DSGN 1",
-  		"id" : "class3"
-  	},
-  	{
-  		"class": "DSGN 100",
-  		"id" : "class4"
-  	},
-  	{
-  		"class": "CSE 87",
-  		"id" : "class5"
-  	},
-
-  	]
-  	});
+  console.log (class_data);  // debug msg
+  res.render("index", class_data);
 };
