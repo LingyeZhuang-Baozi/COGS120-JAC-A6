efbf1353df67866f582998604ff304d9c1bc2ee3
Lingye Zhuang <l1zhuang@ucsd.edu>
2021-02-25 19:57:00 -0800

    sticky highlighter

--- routes/read.js
+++ routes/read.js
@@ -1,19 +1,8 @@
+"use strict";
 
 /*
 * Viewing docs
-*
 */
 exports.viewWords = function(req,res){
   res.render("read");
-}
-
-/*
-* trying ti see if this stored the selected text
-*/
-function selectedText(){
-	var select = window.getSelection().toString();
-}
-function initializePage(){
-	console.log("text selected");
-	selectedText();
 }
