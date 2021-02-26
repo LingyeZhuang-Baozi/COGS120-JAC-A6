36c2e43b0d61636f9acc67a24fc6c6d2b46c6fb2
agathapresa <apresaga@ucsd.edu>
2021-02-25 17:06:08 -0800

    forum button added to reading

--- routes/read.js
+++ routes/read.js
@@ -6,3 +6,14 @@
 exports.viewWords = function(req,res){
   res.render("read");
 }
+
+/*
+* trying ti see if this stored the selected text
+*/
+function selectedText(){
+	var select = window.getSelection().toString();
+}
+function initializePage(){
+	console.log("text selected");
+	selectedText();
+}
